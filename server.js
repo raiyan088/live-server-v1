ba2b2b2c23f199210c7a5f48d5c4ab058129a13d
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-03-05 19:51:54 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening on port 3000...")
+})
+
+updateServer()
+
+setInterval(() => {
+    updateServer()
+}, 5*60*1000)
+
+let mList = []
+
+function updateServer() {
+    request({
+        url: 'https://server-9099-default-rtdb.firebaseio.com/server/live.json',
+        json: true
+    }, function (error, response, body) {
+        let list = []
+        try {
+            if(!error && body) {
+                for (let url of Object.values(body)) {
+                    list.push(url)
+                }
+            }
+        } catch (e) {}
+
+        if (list.length > 0) {
+            mList = list
+        }
+
+        serverLive()
+    })
+}
+
+function serverLive() {
+    for (let i = 0; i < mList.length; i++) {
+        try {
+            console.log(mList[i])
+            request({ url: mList[i] }, function (error, response, body) {})
+        } catch (error) {}
+    }
+}
+
+app.get('/', async function (req, res) {
+    res.end('ok')
+})
